e21fc343228f5e983cf52484c19b18c44db6cf43
Jennifer V <jvu@student.gsu.edu>
2019-02-13 12:52:31 -0500

    added react navigation api

--- Vu_Jennifer/feed_me_app/android/app/src/main/java/com/feed_me_app/MainActivity.java
+++ Vu_Jennifer/feed_me_app/android/app/src/main/java/com/feed_me_app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.feed_me_app;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "feed_me_app";
+    }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
